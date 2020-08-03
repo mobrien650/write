cc9a07be1060df6544f1a3e8402028a2149e6646
Michael OBrien <michael.obrien17@gmail.com>
2020-08-03 10:29:13 -0400

    Updates

---
+++ precache-manifest.dc2a5b59f7a03fd255a815df89d4dd11.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60fa221bf8de87cb48dbaa87b5dfa3b",
+    "url": "/writing/index.html"
+  },
+  {
+    "revision": "d093462f18f4040dedc1",
+    "url": "/writing/static/css/main.6e78ac8b.chunk.css"
+  },
+  {
+    "revision": "8bcc3d0a977a55d6b04a",
+    "url": "/writing/static/js/2.a1dbef78.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/writing/static/js/2.a1dbef78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d093462f18f4040dedc1",
+    "url": "/writing/static/js/main.3a26ff54.chunk.js"
+  },
+  {
+    "revision": "79e69d5555b48e970353",
+    "url": "/writing/static/js/runtime-main.e2d28bb3.js"
+  },
+  {
+    "revision": "f63223c4f20a44ef086b3ebf5eb8ad24",
+    "url": "/writing/static/media/test1.f63223c4.md"
+  },
+  {
+    "revision": "b93d4c5b1c3dbd53393e07ca904b0407",
+    "url": "/writing/static/media/test2.b93d4c5b.md"
+  },
+  {
+    "revision": "3ba1718e213eb94d5e9637204b7643fe",
+    "url": "/writing/static/media/test3.3ba1718e.md"
+  },
+  {
+    "revision": "58d238dd12e6445cdbabf6ccdf55f3ca",
+    "url": "/writing/static/media/test4.58d238dd.md"
+  }
+]);
---
+++ static/media/test4.58d238dd.md
@@ -0,0 +1,5 @@
+# Test Story 4
+
+#### August 1, 2020 ####
+
+This is a test story that goes here.
